c29a8887d8895369fcc7d7c2018c19e1f93f1fac
Mahler, Thomas <thomas.mahler@ista.com>
2020-11-28 18:01:07 +0100

    adding a little example for a heterogenous list of Shape objects.
    Works nicely but we've got a nasty issue with getting the compiler digest the existentials...

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE GADTs #-}
+
 module Lib where
 
 import           Data.Function ((&))
@@ -10,11 +13,13 @@ class Shape a where
   -- | render a Shape
   draw   :: a -> IO ()
   -- | move a Shape by an x and y amount
-  move   :: Double -> Double -> a -> a
+  move   :: (Double,Double) -> a -> a
   -- | compute the area of a Shape
   area   :: a -> Double
   -- | compute the circumference of a Shape
   circum :: a -> Double
+
+
 
 -- | a rectangle defined by to points (bottom left and top right corners) 
 data Rect = Rect Point Point deriving (Show)
@@ -27,8 +32,8 @@ data Triangle = Triangle Point Point Point deriving (Show)
 
 -- | making Rect an instance of Shape
 instance Shape Rect where
-  draw     (Rect a b) = putStrLn $ "Rectangle [" ++ show a ++ ", " ++ show b ++ "]"
-  move x y (Rect a b) = Rect a' b'
+  draw       (Rect a b) = putStrLn $ "Rectangle [" ++ show a ++ ", " ++ show b ++ "]"
+  move (x,y) (Rect a b) = Rect a' b'
     where
       a' = movePoint x y a
       b' = movePoint x y b
@@ -50,15 +55,15 @@ movePoint x y (Point x_a y_a) = Point (x_a + x) (y_a + y)
 
 -- | making Circle an instance of Shape
 instance Shape Circle where
-  draw     (Circle centre radius) = putStrLn $ "Circle [" ++ show centre ++ ", " ++ show radius ++ "]"
-  move x y (Circle centre radius) = Circle (movePoint x y centre) radius
+  draw       (Circle centre radius) = putStrLn $ "Circle [" ++ show centre ++ ", " ++ show radius ++ "]"
+  move (x,y) (Circle centre radius) = Circle (movePoint x y centre) radius
   area   (Circle _ r) = r ^ 2 * pi
   circum (Circle _ r) = 2 * r * pi
 
 -- | making Triangle an instance of Shape
 instance Shape Triangle where
-  draw     (Triangle a b c) = putStrLn $ "Triangle [" ++ show a ++ ", " ++ show b ++ ", " ++ show c ++ "]"
-  move x y (Triangle a b c) = Triangle a' b' c'
+  draw       (Triangle a b c) = putStrLn $ "Triangle [" ++ show a ++ ", " ++ show b ++ ", " ++ show c ++ "]"
+  move (x,y) (Triangle a b c) = Triangle a' b' c'
     where
       a' = movePoint x y a
       b' = movePoint x y b
@@ -83,12 +88,30 @@ distance (Point x_a y_a) (Point x_b y_b) = sqrt ((x_b - x_a) ^ 2 + (y_b - y_a) ^
 instance Show Point where
   show (Point x y) = "(" ++ show x ++ "," ++ show y ++ ")"
 
+data ShapeType = forall a . Shape a => MkShape a
+
+
+instance Shape ShapeType where
+  area     (MkShape a) = area a
+  circum   (MkShape a) = circum a
+  draw     (MkShape a) = draw a
+  move (x,y) (MkShape i) =  undefined
+
+pack :: Shape a => a -> ShapeType
+pack = MkShape
 
 main :: IO ()
 main = do
   let rect = Rect (Point 0 0) (Point 5 4)
       circle = Circle (Point 4 5) 4
       triangle = Triangle (Point 0 0) (Point 4 0) (Point 4 3)
+      list = [pack rect, pack circle, pack triangle]
+
+  print $ map area list
+
+  --mapM_ (draw . move (1, 2)) list
+
+  mapM_ draw list
 
   putStrLn "draw all shapes:"
   draw rect
@@ -97,15 +120,15 @@ main = do
 
   putStrLn "\nnow move all shapes:"
   rect
-    & move 4 2
+    & move (4,2)
     & draw
 
   circle
-    & move 4 2
+    & move (4,2)
     & draw
 
   triangle
-    & move 4 2
+    & move (4,2)
     & draw
 
   putStrLn "\ncompute area of all shapes:"
